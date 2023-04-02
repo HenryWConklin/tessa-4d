d49b34f67f520999396cec2ddd6f14cce877118a
Henry Conklin <henrywconklin@gmail.com>
2023-04-02 12:29:38 -0700

    Simplify mesh api
    * Flatten out Simplex struct
    * Make fields pub and remove getters/setters
    * Per-dimension vertex structs with traits for project/interpolate ops
    * cross_section is now one method, generic accross Vertex structs

--- tessa4d/src/mesh.rs
+++ tessa4d/src/mesh.rs
@@ -1,21 +1,82 @@
 use std::collections::{hash_map::Entry, HashMap};
 
 use crate::{
-    linear_algebra::traits::{Vector, Vector2, Vector3, Vector4},
+    linear_algebra::traits::{Vector2, Vector3, Vector4},
+    prelude::InterpolateWith,
     transform::traits::{Transform, TransformDirection},
     util::lerp,
 };
 
-#[derive(Clone, Copy, Debug)]
-pub struct Vertex<V> {
-    /// Position of the vertex.
+#[derive(Debug, Clone, Copy)]
+pub struct Vertex2<V: Vector2> {
     pub position: V,
-    // TODO Normals and texture coords. Normal in 4D is going to actually be a bivector, so need a general projection and transformation capability.
 }
 
-#[derive(Clone, Copy, Debug)]
-pub struct Simplex<T, const N: usize> {
-    pub vertices: [T; N],
+#[derive(Debug, Clone, Copy)]
+pub struct Vertex3<V: Vector3> {
+    pub position: V,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vertex4<V: Vector4> {
+    pub position: V,
+}
+
+/// Project a vertex to a lower dimension.
+pub trait Project {
+    type Projected;
+    /// This vertex projected to a lower dimension.
+    fn project(&self) -> Self::Projected;
+    /// How far the vertex is from the plane of projection.
+    fn projection_depth(&self) -> f32;
+}
+
+impl<V: Vector3> Project for Vertex3<V> {
+    type Projected = Vertex2<V::Vector2>;
+    fn projection_depth(&self) -> f32 {
+        self.position.z()
+    }
+    fn project(&self) -> Self::Projected {
+        Vertex2 {
+            position: V::Vector2::new(self.position.x(), self.position.y()),
+        }
+    }
+}
+
+impl<V: Vector4> Project for Vertex4<V> {
+    type Projected = Vertex3<V::Vector3>;
+    fn projection_depth(&self) -> f32 {
+        self.position.w()
+    }
+    fn project(&self) -> Self::Projected {
+        Vertex3 {
+            position: V::Vector3::new(self.position.x(), self.position.y(), self.position.z()),
+        }
+    }
+}
+
+impl<V: Vector2> InterpolateWith for Vertex2<V> {
+    fn interpolate_with(&self, other: Self, fraction: f32) -> Self {
+        Self {
+            position: lerp(self.position, other.position, fraction),
+        }
+    }
+}
+
+impl<V: Vector3> InterpolateWith for Vertex3<V> {
+    fn interpolate_with(&self, other: Self, fraction: f32) -> Self {
+        Self {
+            position: lerp(self.position, other.position, fraction),
+        }
+    }
+}
+
+impl<V: Vector4> InterpolateWith for Vertex4<V> {
+    fn interpolate_with(&self, other: Self, fraction: f32) -> Self {
+        Self {
+            position: lerp(self.position, other.position, fraction),
+        }
+    }
 }
 
 /// Generic mesh made of N-simplexes. e.g. a 3-simplex is a triangle, a 4-simplex is a tetrahedron.
@@ -23,9 +84,9 @@ pub struct Simplex<T, const N: usize> {
 pub struct SimplexMesh<V, const N: usize> {
     /// Unique vertices in the tetmesh.
     /// Uniqueness is not required, but it is more efficient.
-    vertices: Vec<Vertex<V>>,
+    pub vertices: Vec<V>,
     /// Indices into the `coordinates` vec representing the vertices of each N-simplex in the mesh.
-    simplexes: Vec<Simplex<usize, N>>,
+    pub simplexes: Vec<[usize; N]>,
 }
 
 pub type Trimesh<V> = SimplexMesh<V, 3>;
@@ -34,46 +95,8 @@ pub type Tetmesh<V> = SimplexMesh<V, 4>;
 impl<V: Copy, const N: usize> SimplexMesh<V, N> {
     pub fn apply_transform<T: Transform<V> + TransformDirection<V>>(&mut self, transform: &T) {
         self.vertices.iter_mut().for_each(|p| {
-            p.position = transform.transform(p.position);
+            *p = transform.transform(*p);
         })
-    }
-    pub fn num_vertices(&self) -> usize {
-        self.vertices.len()
-    }
-    pub fn get_vertex(&self, i: usize) -> Vertex<V> {
-        self.vertices[i]
-    }
-    pub fn get_mut_vertex(&mut self, i: usize) -> &mut Vertex<V> {
-        &mut self.vertices[i]
-    }
-    pub fn num_simplexes(&self) -> usize {
-        self.simplexes.len()
-    }
-    pub fn get_index_simplex(&self, i: usize) -> Simplex<usize, N> {
-        self.simplexes[i]
-    }
-    pub fn get_simplex(&self, i: usize) -> Simplex<Vertex<V>, N> {
-        Simplex {
-            vertices: self.simplexes[i].vertices.map(|ind| self.vertices[ind]),
-        }
-    }
-    pub fn get_mut_index_simplex(&mut self, i: usize) -> &mut Simplex<usize, N> {
-        &mut self.simplexes[i]
-    }
-}
-
-fn project_vertex<V: Vector, W: Vector, F: Fn(V) -> (W, f32)>(
-    vertex: Vertex<V>,
-    project: F,
-) -> Vertex<W> {
-    Vertex {
-        position: project(vertex.position).0,
-    }
-}
-
-fn lerp_vertex<V: Vector>(vertex1: Vertex<V>, vertex2: Vertex<V>, factor: f32) -> Vertex<V> {
-    Vertex {
-        position: lerp(vertex1.position, vertex2.position, factor),
     }
 }
 
@@ -82,31 +105,30 @@ fn lerp_vertex<V: Vector>(vertex1: Vertex<V>, vertex2: Vertex<V>, factor: f32) -
 /// Returns invalid results if both vertices have the same depth, or if they aren't on opposite sides of CROSS_SECTION_DEPTH.
 const TETRAHEDRON_FACE_WINDING: [[usize; 3]; 4] = [[1, 3, 2], [0, 2, 3], [0, 3, 1], [0, 1, 2]];
 const CROSS_SECTION_DEPTH: f32 = 0.0;
-fn project_edge<V: Vector, W: Vector, F: Fn(V) -> (W, f32) + Copy>(
-    vertex1: Vertex<V>,
-    vertex2: Vertex<V>,
-    project: F,
-) -> Vertex<W> {
-    let (_, depth1) = project(vertex1.position);
-    let (_, depth2) = project(vertex2.position);
+fn project_edge<V: Project>(vertex1: V, vertex2: V) -> V::Projected
+where
+    V::Projected: InterpolateWith,
+{
+    let depth1 = vertex1.projection_depth();
+    let depth2 = vertex2.projection_depth();
     let intersection = depth1 / (depth1 - depth2);
-    let vertex1 = project_vertex(vertex1, project);
-    let vertex2 = project_vertex(vertex2, project);
-    lerp_vertex(vertex1, vertex2, intersection)
+    let vertex1 = vertex1.project();
+    let vertex2 = vertex2.project();
+    vertex1.interpolate_with(vertex2, intersection)
 }
 
-impl<V: Vector> Tetmesh<V> {
+impl<V: Project + Copy> Tetmesh<V>
+where
+    V::Projected: InterpolateWith,
+{
     /// Returns the cross section of this mesh using `project` to map to another vector space.
     /// `project` must perform a linear mapping to another vector space and return a "depth" value,
     /// the returned cross section will then be the set of mapped points with depth=0.
     /// Preserves the handedness (winding order) of the source mesh in the resulting mesh, so that a clockwise tetrahedron gives clockwise triangles.
-    fn cross_section<W: Vector, F>(self, project: F) -> Trimesh<W>
-    where
-        F: Fn(V) -> (W, f32) + Copy,
-    {
+    pub fn cross_section(self) -> Trimesh<V::Projected> {
         // Maps edges in the old mesh to projected vertices in the new mesh, takes the edge as a tuple with the lower index first.
         let mut edge_indices: HashMap<(usize, usize), usize> = HashMap::new();
-        let mut projected_vertices: Vec<Vertex<W>> = vec![];
+        let mut projected_vertices: Vec<V::Projected> = vec![];
         // Returns the index of the intersection point in the new mesh for the edge between the given vertices in the old mesh.
         let mut get_intersection = |i: usize, j: usize| {
             let key = (i.min(j), i.max(j));
@@ -115,7 +137,7 @@ impl<V: Vector> Tetmesh<V> {
                 Entry::Vacant(slot) => {
                     let vertex1 = self.vertices[i];
                     let vertex2 = self.vertices[j];
-                    let projected_vertex = project_edge(vertex1, vertex2, project);
+                    let projected_vertex = project_edge(vertex1, vertex2);
                     projected_vertices.push(projected_vertex);
                     let index = projected_vertices.len() - 1;
                     slot.insert(index);
@@ -127,8 +149,8 @@ impl<V: Vector> Tetmesh<V> {
             .simplexes
             .into_iter()
             .flat_map(|simplex| {
-                let vertex_section_side = simplex.vertices.map(|vert_index| {
-                    project(self.vertices[vert_index].position).1 > CROSS_SECTION_DEPTH
+                let vertex_section_side = simplex.map(|vert_index| {
+                    self.vertices[vert_index].projection_depth() > CROSS_SECTION_DEPTH
                 });
                 // One vertex on negative side, use face winding order. Takes index of the one negative-depth vertex.
                 let one_negative_case =
@@ -165,16 +187,14 @@ impl<V: Vector> Tetmesh<V> {
                     [false, false, true, true] => two_negative_case(0, 1, 2, 3),
                     [true, true, false, false] => two_negative_case(3, 2, 1, 0),
                     [true, false, true, false] => two_negative_case(1, 3, 0, 2),
-                    [false, true, false, true] => dbg!(two_negative_case(0, 2, 3, 1)),
+                    [false, true, false, true] => two_negative_case(0, 2, 3, 1),
                     [true, false, false, true] => two_negative_case(2, 1, 3, 0),
                     [false, true, true, false] => two_negative_case(0, 3, 1, 2),
                 };
-                dbg!(faces)
+                faces
                     .into_iter()
-                    .map(|face_edges| Simplex {
-                        vertices: face_edges.map(|(i, j)| {
-                            get_intersection(simplex.vertices[i], simplex.vertices[j])
-                        }),
+                    .map(|face_edges| {
+                        face_edges.map(|(i, j)| get_intersection(simplex[i], simplex[j]))
                     })
                     .collect::<Vec<_>>()
             })
@@ -183,20 +203,6 @@ impl<V: Vector> Tetmesh<V> {
             vertices: projected_vertices,
             simplexes: projected_simplexes,
         }
-    }
-}
-impl<V: Vector4> Tetmesh<V> {
-    /// 3D cross section of a 4D tetrahedral mesh at w = 0
-    pub fn cross_section_4d(self) -> Trimesh<V::Vector3> {
-        // Note, can't use the same name for Vector3 and Vector4 case because something could implement both Vector3 and Vector4, so compiler throws an error about conflicting implementations.
-        // Also, relying on the code generalizing between dimensions, no good intuition for how winding order in particular should work going from 4D to 3D.
-        self.cross_section(|x| (V::Vector3::new(x.x(), x.y(), x.z()), x.w()))
-    }
-}
-impl<V: Vector3> Tetmesh<V> {
-    /// 2D cross section of a 3D tetrahedral mesh at z = 0.
-    pub fn cross_section_3d(self) -> Trimesh<V::Vector2> {
-        self.cross_section(|x| (V::Vector2::new(x.x(), x.y()), x.z()))
     }
 }
 
@@ -210,13 +216,14 @@ mod test {
     fn project_edge_returns_intersection() {
         let vertex1 = make_vertex_3d(1.0, 0.0, -0.2);
         let vertex2 = make_vertex_3d(0.0, 1.0, 0.8);
-        let project = |v: glam::Vec3| (glam::vec2(v.x, v.y), v.z);
-        let expected = glam::vec2(0.8, 0.2);
+        let expected = Vertex2 {
+            position: glam::vec2(0.8, 0.2),
+        };
         dbg!(expected);
 
-        let got = dbg!(project_edge(vertex1, vertex2, project));
+        let got = dbg!(project_edge(vertex1, vertex2));
 
-        assert!(got.position.abs_diff_eq(expected, EPS));
+        assert!(got.position.abs_diff_eq(expected.position, EPS));
     }
 
     macro_rules! one_three_split_tests {
@@ -233,16 +240,16 @@ mod test {
                             make_vertex_3d(0.0, 0.0, -1.0),
                             make_vertex_3d(0.0, 2.0, -1.0),
                         ],
-                        simplexes: vec![make_simplex($tet_winding)],
+                        simplexes: vec![$tet_winding],
                     };
                     let tetmesh = dbg!(tetmesh);
 
-                    let got = dbg!(tetmesh.cross_section_3d());
+                    let got = dbg!(tetmesh.cross_section());
 
-                    assert_eq!(got.num_simplexes(), 1);
-                    assert_eq!(got.num_vertices(), 3);
-                    let simplex = got.get_simplex(0);
-                    assert!(triangle_is_clockwise(simplex));
+                    assert_eq!(got.simplexes.len(), 1);
+                    assert_eq!(got.vertices.len(), 3);
+                    let simplex = got.simplexes[0];
+                    assert!(triangle_is_clockwise(simplex.map(|i| got.vertices[i].position)));
                 }
                 )*
             }
@@ -258,17 +265,17 @@ mod test {
                             make_vertex_3d(0.0, 0.0, 1.0),
                             make_vertex_3d(0.0, 2.0, 1.0),
                         ],
-                        simplexes: vec![make_simplex($tet_winding)],
+                        simplexes: vec![$tet_winding],
                     };
                     let tetmesh = dbg!(tetmesh);
 
-                    let got = dbg!(tetmesh.cross_section_3d());
+                    let got = dbg!(tetmesh.cross_section());
 
-                    assert_eq!(got.num_simplexes(), 1);
-                    assert_eq!(got.num_vertices(), 3);
-                    let simplex = got.get_simplex(0);
+                    assert_eq!(got.simplexes.len(), 1);
+                    assert_eq!(got.vertices.len(), 3);
+                    let simplex = got.simplexes[0];
                     // Should be opposite of the one positive case because the coords are mirrored so handedness is flipped.
-                    assert!(!triangle_is_clockwise(simplex));
+                    assert!(!triangle_is_clockwise(simplex.map(|i| got.vertices[i].position)));
                 }
                 )*
             }
@@ -296,16 +303,16 @@ mod test {
                                 make_vertex_3d(0.0, 0.0, -1.0),
                                 make_vertex_3d(0.0, 2.0, -1.0),
                             ],
-                            simplexes: vec![make_simplex($tet_winding)],
+                            simplexes: vec![$tet_winding],
                         };
                         let tetmesh = dbg!(tetmesh);
 
-                        let got = dbg!(tetmesh.cross_section_3d());
+                        let got = dbg!(tetmesh.cross_section());
 
-                        assert_eq!(got.num_simplexes(), 2);
-                        assert_eq!(got.num_vertices(), 4);
-                        assert!(triangle_is_clockwise(got.get_simplex(0)));
-                        assert!(triangle_is_clockwise(got.get_simplex(1)));
+                        assert_eq!(got.simplexes.len(), 2);
+                        assert_eq!(got.vertices.len(), 4);
+                        assert!(triangle_is_clockwise(got.simplexes[0].map(|i| got.vertices[i].position)));
+                        assert!(triangle_is_clockwise(got.simplexes[1].map(|i| got.vertices[i].position)));
                     }
                 )*
             }
@@ -320,19 +327,14 @@ mod test {
         v2v3: [3, 2, 1, 0],
     }
 
-    fn make_vertex_3d(x: f32, y: f32, z: f32) -> Vertex<glam::Vec3> {
-        Vertex {
+    fn make_vertex_3d(x: f32, y: f32, z: f32) -> Vertex3<glam::Vec3> {
+        Vertex3 {
             position: glam::vec3(x, y, z),
         }
     }
 
-    fn make_simplex<const N: usize>(inds: [usize; N]) -> Simplex<usize, N> {
-        Simplex { vertices: inds }
-    }
-
-    fn triangle_is_clockwise(simplex: Simplex<Vertex<glam::Vec2>, 3>) -> bool {
-        let wedge = (simplex.vertices[0].position - simplex.vertices[1].position)
-            .perp_dot(simplex.vertices[2].position - simplex.vertices[1].position);
+    fn triangle_is_clockwise(simplex: [glam::Vec2; 3]) -> bool {
+        let wedge = (simplex[0] - simplex[1]).perp_dot(simplex[2] - simplex[1]);
         dbg!(wedge);
         wedge > 0.0
     }
