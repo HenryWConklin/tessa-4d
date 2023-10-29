52408e4cbac5d8b936033dfc3ab5a3085017ffa2
Henry Conklin <henrywconklin@gmail.com>
2023-10-29 14:21:09 -0400

    Add status messages to run-checks.sh

--- run-checks.sh
+++ run-checks.sh
@@ -5,10 +5,13 @@ CARGO_PROJECTS=('tessa4d' 'tessa4d-bevy' 'tessa4d-gdext')
 
 # Run tests first so formatting/lints don't block it.
 for project in "${CARGO_PROJECTS[@]}"; do
+  echo "Test ${project}"
   cargo test --package "$project"
 done
 
 for project in "${CARGO_PROJECTS[@]}"; do
+  echo "Clippy ${project}"
   cargo clippy --package "$project" --no-deps
+  echo "Rustfmt ${project}"
   cargo fmt --check --package "$project"
 done
